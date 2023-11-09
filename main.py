5c50536e3008013452cf3072d8bec15ee112a230
pavsteruk <pavelnaumovic@gmail.com>
2023-11-09 21:44:42 +0000

    updated

--- main.py
+++ main.py
@@ -2,11 +2,14 @@ from flask import Flask
 from app.audio_transcription import transcription_blueprint
 from app.diarization import diarization_blueprint
 from app.text_analysis import analysis_blueprint
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)  # Enable CORS for the entire app
+
 app.register_blueprint(transcription_blueprint)
 app.register_blueprint(diarization_blueprint)
 app.register_blueprint(analysis_blueprint)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8000, debug=True)
